b7582b5ee70cdb121e797cc01f9e189a3dd5d9cf
John Richardson <jmrichardson@gmail.com>
2024-11-03 18:48:40 -0500

    Update meta_model.py

--- numerblox/meta_model.py
+++ numerblox/meta_model.py
@@ -38,7 +38,7 @@ def get_sample_weights(data, wfactor=0.2, eras=None):
 
 
 class MetaModel(BaseEstimator, RegressorMixin):
-    def __init__(self, max_ensemble_size: int = 10, num_bags = 50, random_state: int = 42, weight_factor: float = None, metric='corr'):
+    def __init__(self, max_ensemble_size: int = 10, num_bags=50, random_state: int = 42, weight_factor: float = None, metric='corr'):
         self.metric = metric
         self.max_ensemble_size = max_ensemble_size
         self.num_bags = num_bags
@@ -47,12 +47,7 @@ class MetaModel(BaseEstimator, RegressorMixin):
         self.selected_model_names = []  # List to store the names of selected models
         self.ensemble_model = None
 
-    def fit(
-            self,
-            oof,
-            models,
-            ensemble_method=GreedyEnsemble,
-    ):
+    def fit(self, oof, models, ensemble_method=GreedyEnsemble):
 
         if oof.isnull().values.any():
             raise ValueError("Out of fold predictions contains NaN values.")
@@ -63,10 +58,18 @@ class MetaModel(BaseEstimator, RegressorMixin):
         if not isinstance(models, dict):
             raise TypeError("The 'models' variable must be a dictionary.")
 
+        # Scale oof predictions to [0, 1]
+        oof_predictions = oof.drop(columns=['era', 'target', 'meta_data'], errors='ignore')
+        oof_predictions = oof_predictions.apply(lambda x: (x - x.min()) / (x.max() - x.min()), axis=0)
+
+        # Reinsert scaled predictions into oof DataFrame
+        oof_scaled = oof.copy()
+        oof_scaled.update(oof_predictions)
+
         # If weight_factor is provided, calculate sample weights based on eras
         if self.weight_factor is not None:
             logger.info(f"Generating sample weights - Weight factor: {self.weight_factor}")
-            sample_weights = get_sample_weights(oof.drop(columns=['era', 'target', 'meta_data'], errors='ignore'), wfactor=self.weight_factor, eras=oof.era)
+            sample_weights = get_sample_weights(oof_scaled.drop(columns=['era', 'target', 'meta_data'], errors='ignore'), wfactor=self.weight_factor, eras=oof_scaled['era'])
         else:
             sample_weights = None
 
@@ -74,9 +77,9 @@ class MetaModel(BaseEstimator, RegressorMixin):
         if isinstance(ensemble_method, type):
             ensemble_method = ensemble_method(max_ensemble_size=self.max_ensemble_size, num_bags=self.num_bags, metric=self.metric, random_state=self.random_state)
 
-        # Fit the ensemble method with base model predictions, true targets, and sample weights
+        # Fit the ensemble method with scaled predictions, true targets, and sample weights
         logger.info(f"Ensembling out of sample predictions - Metric: {self.metric}")
-        ensemble_method.fit(oof, sample_weights=sample_weights)
+        ensemble_method.fit(oof_scaled, sample_weights=sample_weights)
 
         # Get the names of the selected models and their weights
         self.selected_model_names = ensemble_method.selected_model_names_
@@ -94,17 +97,14 @@ class MetaModel(BaseEstimator, RegressorMixin):
         # Prepare the final VotingRegressor ensemble model
         weights_list = []
         estimators_list = []
-        models = []
         for model_name, model in selected_models:
             weight = self.weights_.loc[model_name]  # Get the weight for the model
             weights_list.append(weight)  # Add the weight to the list
             logger.info(f"Adding model - Model: {model_name}, Weight: {weight}")
             estimators_list.append((model_name, model))  # Add the model and its name to the list
-            models.append(model)
 
         # Create the VotingRegressor with the selected models and their corresponding weights
         self.ensemble_model = VotingRegressor(estimators=estimators_list, weights=weights_list)
-        self.ensemble_model.estimators_ = models
 
         return self
 
